d173c12e6ddc9c1a7fcfdbc47d20494556bf1edc
cjqw <2462457751@qq.com>
2017-03-18 17:44:54 +0800

    add unittest for constant

--- test.py
+++ test.py
@@ -12,15 +12,21 @@ class utilTestCase(unittest.TestCase):
         self.onev = [1 for i in range(0,10)]
         self.incv = [i for i in range(0,10)]
         self.randv = [random() for i in range(0,10)]
+        self.x = random()
 
     def test_identity(self):
         # Test when the input is a double
-        x = random()
-        assert x == (identity())(x)
+        assert self.x == (identity())(self.x)
         # Test when the input is a vector
         for i in range(0,len(self.randv)):
             assert self.randv[i] == (identity())(self.randv)[i]
         assert len(self.randv) == len(identity()(self.randv))
+
+    def test_constant(self):
+        # Test when input is a double
+        f = constant(self.x)
+        assert f('a') == self.x
+        assert f('a',incv) == self.x
 
 if __name__ == '__main__':
     unittest.main()
