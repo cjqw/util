af883f1c057e6820d57f07492f417ec06dd5486e
cjqw <2462457751@qq.com>
2017-03-31 23:23:35 +0800

    add cd class

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import os
 from functools import reduce,partial
 
 def identity(x):
@@ -56,3 +57,18 @@ def partition(v,f = identity):
         else:
             result.update({key:[item]})
     return result
+
+# This class comes from stackOverFlow
+# http://stackoverflow.com/questions/431684/how-do-i-cd-in-python
+# Thanks to Brian M. Hunt
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
